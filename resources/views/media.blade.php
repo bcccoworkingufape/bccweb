4c65735f71f80259781c7ac9340e9e39e3717208
Erik Jhonatta <erikjhonatta@gmail.com>
2021-08-30 18:20:36 -0300

    fix:addon http

--- resources/views/media.blade.php
+++ resources/views/media.blade.php
@@ -4,6 +4,6 @@
 @endsection
 @section("content")
 <div style="margin-bottom: 13px;font-size: 24px; font-weight: 700; width: 144px;"><a class="nav-link" href="https://www.instagram.com/bccufape/" target='_blank'>@bccufape</a></div>
-<!-- LightWidget WIDGET --><script src="http://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/cb05b3c56bd75794a7c755e0f0838b5c.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+<!-- LightWidget WIDGET --><script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/cb05b3c56bd75794a7c755e0f0838b5c.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
 
 @endsection
