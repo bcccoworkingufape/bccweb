038671827d8922034057317173c44df6a1006c1b
ErikJhonatta <erikjhonatta@gmail.com>
2021-09-09 19:55:32 -0300

    feat: Adding sheet visualization

--- resources/views/remote-classes.blade.php
+++ resources/views/remote-classes.blade.php
@@ -7,11 +7,12 @@
     <div class="remote-class">ENSINO REMOTO</div>
     <div>
         <ul class="links-container">
-            <li><a href="https://drive.google.com/file/d/1KMrBoK3FxKui7EMz5kzFQsEu9VUr-nqz/view" target="_blank">Resolução ensino remoto</a></li>
-            <li><a
-                    href="https://docs.google.com/spreadsheets/d/1tJLUqA1wRpgsXSfb6SQ4IkJYGwGGRddhjye8NTVDxU0/edit?urp=gmail_link#gid=0" target="_blank">Salas Virtuais de Ensino</a></li>
-            <li><a
-                    href="http://ufape.edu.br/br/funcionamento-administrativo-laborat%C3%B3rios-durante-per%C3%ADodo-letivo-20201" target="_blank">Funcionamento administrativo e de laboratórios durante o Período Letivo 2020.1</a></li>
+        <li><a href="https://docs.google.com/spreadsheets/d/1tJLUqA1wRpgsXSfb6SQ4IkJYGwGGRddhjye8NTVDxU0/edit?urp=gmail_link#gid=0" target="_blank">Salas Virtuais de Ensino</a></li>
+        <div class="sheet" style="height:300px; width:100%;">
+            <iframe height="300px" width="100%" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSDbBliBNPHa9hZlDlDRKjQJ-CfOouLabyqbETfGWAbL3QJWC41khqUXSbdInGwZCzMZQyRlKIjq6DR/pubhtml?gid=0&amp;single=true&amp;chrome=false&amp;widget=false&amp;headers=false"></iframe>
+        </div>
+        <li><a href="https://drive.google.com/file/d/1KMrBoK3FxKui7EMz5kzFQsEu9VUr-nqz/view" target="_blank">Resolução ensino remoto</a></li>
+        <li><a href="http://ufape.edu.br/br/funcionamento-administrativo-laborat%C3%B3rios-durante-per%C3%ADodo-letivo-20201" target="_blank">Funcionamento administrativo e de laboratórios durante o Período Letivo 2020.1</a></li>
         </ul>
     </div>
 @endsection
