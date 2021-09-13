74f073335e34fa08a7542aa2685a503f45ad7161
ErikJhonatta <erikjhonatta@gmail.com>
2021-09-13 11:55:54 -0300

    fix: increasing teachers pagination

--- app/Http/Controllers/TeachersController.php
+++ app/Http/Controllers/TeachersController.php
@@ -280,7 +280,7 @@ class TeachersController extends Controller
     }';
     public function index(){
         $data = json_decode($this->data);
-        $data = $this->paginate($data,9);
+        $data = $this->paginate($data,15);
         return view('teachers.index',['data'=>$data]);//TODO pass all the teachers to this view when the backend is built
     }
 
