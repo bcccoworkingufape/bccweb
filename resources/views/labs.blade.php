159c5baf99fba1267a7829410c88dbb98e5a0a74
David Brito <david.nrpg@gmail.com>
2021-09-14 14:58:26 -0300

    Update: Updating labs logos and text

--- resources/views/labs.blade.php
+++ resources/views/labs.blade.php
@@ -10,7 +10,7 @@
 
     <li class="labs-item">
         <header>
-            <img src="img/logo_bcccoworking.png" alt="logo_bcc" /></a>
+            <img src="img/lab13.png" alt="lab_13" /></a>
             <div class="labs-info">
                 <strong>Laboratório 13</strong>{{--nome!--}}</a>
             </div>
@@ -22,29 +22,32 @@
 
     <li class="labs-item">
         <header>
-            <img src="img/logo_bcccoworking.png" alt="logo_bcc" /></a>
+            <img src="img/labss.png" alt="labs" /></a>
             <div class="labs-info">
                 <strong>Laboratórios do Curso</strong>{{--nome!--}}</a>
             </div>
         </header>
         <p>
-            Laboratórios 10, 11, 12, 21, 22, 23 e 24 - Disponíveis para aulas e práticas pedagógicas no geral
+            Laboratórios 10, 11, 12, 21, 22 e 23 - Disponíveis para aulas e práticas pedagógicas no geral
         </p>
         <a  target="_blank" href="http://sistemas.ufape.edu.br/reserva/">Solicitação de Agendamento</a>
     </li>
 
     <li class="labs-item">
         <header>
-            <img src="img/logo_bcccoworking.png" alt="logo_bcc" /></a>
+            <img src="img/lab24.png" alt="lab_24" /></a>
             <div class="labs-info">
-                <strong>Laboratórios de Redes e Sistemas Distribuídos</strong>{{--nome!--}}</a>
+                <strong>Laboratório de Pesquisa e Projetos</strong>{{--nome!--}}</a>
             </div>
         </header>
+        <p>
+            Laboratório 24
+        </p>
     </li>
 
     <li class="labs-item">
         <header>
-            <img src="img/logo_bcccoworking.png" alt="logo_bcc" /></a>
+            <img src="img/logo-uname.png" alt="logo_uname" /></a>
             <div class="labs-info">
                 <strong>Uname Lab</strong>{{--nome!--}}</a>
             </div>
@@ -58,19 +61,19 @@
         <header>
             <img src="img/logo_lmts.png" alt="logo_lmts" /></a>
             <div class="labs-info">
-                <a  target="_blank" href="http://lmts.ufape.edu.br/"><strong>LMTS</strong>{{--nome!--}}</a>
+                <a  target="_blank" href="http://lmts.ufape.edu.br/"><strong>LMTS – Laboratório Multidisciplinar de Tecnologias Sociais</strong>{{--nome!--}}</a>
             </div>
         </header>
-        <a href="http://lmts.ufape.edu.br/" target="_blank">Saiba mais</a>
+        <a href="http://lmts.ufape.edu.br/" target="_blank">http://lmts.ufape.edu.br/</a>
     </li>
 
     <li class="labs-item">
         <header>
-            <img src="img/logo_bcccoworking.png" alt="logo_bcccoworking" />
+            <img src="img/logo-bcccoworking-menor.jpeg" alt="logo_bcccoworking" />
             <div class="labs-info">
-                <a  target="_blank" href="http://app.uag.ufrpe.br/bcccoworking/home"><strong>BCC Coworking</strong>{{--nome!--}}</a>
+                <a  target="_blank" href="http://app.uag.ufrpe.br/bcccoworking/home"><strong>Laboratório de Pesquisa e Desenvolvimento – BCC Coworking</strong>{{--nome!--}}</a>
             </div>
         </header>
-        <a  target="_blank" href="http://app.uag.ufrpe.br/bcccoworking/">Saiba mais</a>
+        <a  target="_blank" href="http://app.uag.ufrpe.br/bcccoworking/">http://app.uag.ufrpe.br/bcccoworking/</a>
     </li>
 @endsection
