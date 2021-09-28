c954b0a827d83c3d53f256feaa98fd09482b8216
ErikJhonatta <erikjhonatta@gmail.com>
2021-09-28 19:44:52 -0300

    feat: Adding link of remote classes

--- resources/views/remote-classes.blade.php
+++ resources/views/remote-classes.blade.php
@@ -7,6 +7,7 @@
     <div class="remote-class">ENSINO REMOTO</div>
     <div>
         <ul class="links-container">
+        <li><a href="http://ufape.edu.br/br/noticia/ufape-ufrpe-aprovam-plano-funcionamento-2021-calend%C3%A1rio-letivo-2020" target="_blank">Plano de Funcionamento do Ensino Remoto</a></li>
         <li><a href="https://docs.google.com/spreadsheets/d/1tJLUqA1wRpgsXSfb6SQ4IkJYGwGGRddhjye8NTVDxU0/edit?urp=gmail_link#gid=0" target="_blank">Salas Virtuais de Ensino</a></li>
         <li><a href="https://drive.google.com/file/d/1KMrBoK3FxKui7EMz5kzFQsEu9VUr-nqz/view" target="_blank">Resolução 273/2021 UFAPE/UFRPE</a></li>
         <li><a href="http://ufape.edu.br/br/funcionamento-administrativo-laborat%C3%B3rios-durante-per%C3%ADodo-letivo-20201" target="_blank">Funcionamento administrativo e de laboratórios durante o Período Letivo 2020.1</a></li>
