2ec048fe0509936a379635caae2723a1cfbda2e9
David Brito <david.nrpg@gmail.com>
2021-09-29 17:36:05 -0300

    Update: Changing labs index view

--- resources/views/labs/index.blade.php
+++ resources/views/labs/index.blade.php
@@ -10,73 +10,74 @@
 
     <li class="labs-item">
         <header>
+            <a href="/laboratorios-bcc" target="_blank"><img src="img/lab13.png" alt="lab_13" /></a>
             <div class="labs-info">
                 <a href="/laboratorios-bcc" target="_blank"><strong>Laboratório de Estudo (Laboratório 13)</strong>{{--nome!--}}</a>
             </div>
         </header>
         <p>
-            <a href="/laboratorios-bcc" target="_blank"><img src="img/lab13.png" alt="lab_13" /></a>
             Compartilhado para estudo e atividades individuais e em equipe
         </p>
     </li>
 
     <li class="labs-item">
         <header>
+            <a href="/laboratorios-bcc" target="_blank"><img src="img/labss.png" alt="labs" /></a>
             <div class="labs-info">
                 <a href="/laboratorios-bcc" target="_blank"><strong>Laboratórios de Aulas Práticas (Laboratórios 10, 11, 12, 21, 22 e 23)</strong>{{--nome!--}}</a>
             </div>
         </header>
         <p>
-            <a href="/laboratorios-bcc" target="_blank"><img src="img/labss.png" alt="labs" /></a>
             Disponíveis para aulas e práticas pedagógicas no geral
         </p>
     </li>
 
     <li class="labs-item">
         <header>
+            <a href="/laboratorios-bcc" target="_blank"><img src="img/lab24.png" alt="lab_24" /></a>
             <div class="labs-info">
                 <a href="/laboratorios-bcc" target="_blank"><strong>Laboratório de Pesquisa e Projetos (Laboratório 24)</strong>{{--nome!--}}</a>
             </div>
         </header>
         <p>
-            <a href="/laboratorios-bcc" target="_blank"><img src="img/lab24.png" alt="lab_24" /></a>
             Compartilhado para Projetos de Pesquisa e Desenvolvimento
         </p>
     </li>
 
     <li class="labs-item">
         <header>
-
+            <img src="img/logo-uname.png" alt="logo_uname" />
             <div class="labs-info">
                 <strong>Uname Lab</strong>{{--nome!--}}</a>
             </div>
         </header>
         <p>
-            <img src="img/logo-uname.png" alt="logo_uname" /></a>UNAME is for Network, Availability, Modeling and Experiments
+            </a>UNAME is for Network, Availability, Modeling and Experiments
         </p>
     </li>
 
     <li class="labs-item">
         <header>
-
-            <div class="labs-info">
-                <a  target="_blank" href="http://lmts.ufape.edu.br/"><strong>LMTS – Laboratório Multidisciplinar de Tecnologias Sociais</strong>{{--nome!--}}</a>
-            </div>
-        </header>
-        <p>
             <a href="http://lmts.ufape.edu.br/" target="_blank"><img src="img/logo_lmts_2.png" alt="logo_lmts" /></a>
+            <div class="labs-info">
+                <a  target="_blank" href="http://lmts.ufape.edu.br/"><strong>LMTS</strong>{{--nome!--}}</a>
+            </div>
+        </header>
+        <p>
+            Laboratório Multidisciplinar de Tecnologias Sociais
         </p>
 
     </li>
 
     <li class="labs-item">
         <header>
+            <a  target="_blank" href="http://app.uag.ufrpe.br/bcccoworking/"> <img src="img/logo-bcccoworking-menor.jpeg" alt="logo_bcccoworking" /></a>
             <div class="labs-info">
-                <a  target="_blank" href="http://app.uag.ufrpe.br/bcccoworking/home"><strong>Laboratório de Pesquisa e Desenvolvimento – BCC Coworking</strong>{{--nome!--}}</a>
+                <a  target="_blank" href="http://app.uag.ufrpe.br/bcccoworking/home"><strong>BCC Coworking</strong>{{--nome!--}}</a>
             </div>
         </header>
         <p>
-            <a  target="_blank" href="http://app.uag.ufrpe.br/bcccoworking/"> <img src="img/logo-bcccoworking-menor.jpeg" alt="logo_bcccoworking" /></a>
+            Laboratório de Pesquisa e Desenvolvimento
         </p>
 
     </li>
