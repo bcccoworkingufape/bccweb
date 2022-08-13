74f7897e23799e7d316f41668ff2d010f763ea49
ErikJhonatta <erikjhonatta@gmail.com>
2022-08-13 18:45:42 -0300

    Adding CMS API Handler

--- resources/views/documents.blade.php
+++ resources/views/documents.blade.php
@@ -26,17 +26,9 @@
         <div class="documents-container">
             <strong>Anexos</strong>
             <ul>
-                <li><strong>Resoluções</strong></li>
-                <ul>
-                    <li><a href="https://drive.google.com/file/d/13Pf_fm4AYWIM9wHvgd9uhl5MRJXBuMUE/view" target="_blank">Estágio Supervisionado Obrigatório (ESO)</li>
-                    <li><a href="https://drive.google.com/file/d/1-tvDLr1QlhDOcBd3nODr1DAPuJItPlks/view" target="_blank">Trabalho de Conclusão do Curso (TCC)</a></li>
-               </ul>
-                <li><a href="https://drive.google.com/file/d/1ar9GSqJOTYUOHIMmEoDbPg_syhIbB53k/view" target="_blank">Projeto Pedagógico do Curso (PPC)</a></li>
-                <li><a href="https://drive.google.com/file/d/1A9ICUxQp4Y2OZ0C4kDVt6L9BGF3AeN-V/view" target="_blank">Matriz Curricular</a></li>
-                <li><a href="https://drive.google.com/file/d/1Ykh2uN1ldkuFunrDIjMb1Ioww03mH0y4/view?usp=sharing" target="_blank">Horário do Semestre (2021.2)</a></li>
-                <li><a href="http://ufape.edu.br/sites/temaunidades.ufrpe.br/files/comissoes/Resolu%C3%A7%C3%A3o%20CONSU_UFAPE%20015-2020.pdf" target="_blank">Nova Resolução de Atividades Complementares</a></li>
-
-                <!-- <li>Manual do Discente</li> -->
+            @foreach($data as $doc)
+                <li><a href="{{$doc->path}}" target="_blank">{{$doc->titulo}}</a></li>
+            @endforeach
             </ul>
 
         </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,30 +5,29 @@
 @section('content')
 <div style="margin-bottom: 13px;font-size: 24px; font-weight: 700;">NOTÍCIAS</div>
 
-<div class="form-row">
+<div class="form-row" style="margin-top:25px;">
     @foreach($data as $news)
     @if($loop->index < 4) <div class="style_card_destaque_principal">
         <div class="" style="margin: 5px;position:relative;">
             <div class="newscont" style="background-color: #F6F6F6;border-radius: 12px; padding-bottom: 25px;position:relative;">
-                <a href="{{route('news.show',['id'=>$news->title])}}"><img style="max-height:215px;object-fit: contain;" class="card-img-top" src="{{$news->photo}}" alt="Card image cap" style=" border-top-left-radius: 12px;border-top-right-radius: 12px;"></a>
+                <a href="{{route('news.show',['id'=>$news->id])}}"><img style="max-height:215px;object-fit: contain;" class="card-img-top" src="{{$news->img}}" alt="Card image cap" style=" border-top-left-radius: 12px;border-top-right-radius: 12px;"></a>
                 <div class="container" style="padding-top: 10px;;">
                     <div class="row">
                         <div class="col-md-12">
                             <div class="row">
                                 <div class="title col-md-12" style="font-size: 20px; font-family: Arial, Helvetica, sans-serif; font-weight: 500; line-height: 20px;color:black; margin-top: 10px; margin-bottom: 10px;">
-                                    <a style="color:black;" href="{{route('news.show',['id'=>$news->title])}}">{{$news->title}}</a>
+                                    <a style="color:black;" href="{{route('news.show',['id'=>$news->id])}}">{{$news->titulo}}</a>
                                 </div>
                                 <div class="desc col-md-12" style="font-size: 15px;line-height: 19px;color: #707070; margin-bottom: 10px;">
-                                    {!! $news->description !!}
+                                    {!! $news->descricao !!}
                                 </div>
                                 <div class="col-md-12" style="font-size: 15px;line-height: 19px;color: #707070; margin-bottom: 10px; position:absolute;bottom:-50px;">
                                     <img src="img/icone_calendario.png" alt="" width="17px" style="margin-top: -4px; margin-right: 3px;">
-                                    <span>{{$news->date}}</span>
+                                    <span>{{substr($news->created_at,0,10)}}</span>
                                 </div>
                                 <div class="col-md-12">
                                     <hr style="position:relative; bottom:-15px; margin-top: -6px; margin-bottom: -6px;">
                                 </div>
-                                <!-- <div class="col-md-12">Imagem/Nome do professor</div> -->
                             </div>
                         </div>
                     </div>
@@ -46,6 +45,18 @@
         <div style="margin-bottom: 13px;font-size: 24px; font-weight: 700;">EVENTOS</div>
     </div>
     <div class="row" style="height: calc(49%);">
+        @foreach(array_slice($eventos,0,5) as $evento)
+        <div class="col-md-12" style="margin-bottom: 10px;">
+            <div class="btn-group" style="background-color:#F9F9F9;width: 100%;border-radius: 12px;">
+                <div class="event-number">{{$loop->iteration}}º</div>
+                <div class="form-group" style="margin-top: 10px;">
+                    <div class="textDiv" style="font-weight: 600; color: #1492E6;">{{$evento->titulo}}</div>
+                    <div class="textDiv" style="font-size: 12px;width: 95%;color: #707070;">{{$evento->data}}</div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+{{--
         <div class="col-md-12" style="margin-bottom: 10px;">
             <div class="btn-group" style="background-color:#F9F9F9;width: 100%;border-radius: 12px;">
                 <div class="event-number">1º</div>
@@ -90,7 +101,7 @@
                     <div class="textDiv" style="font-size: 12px;width: 95%;color: #707070;">14-18/02/2022</div>
                 </div>
             </div>
-        </div>
+        </div> --}}
 
     </div>
     <div class="container_labs">
